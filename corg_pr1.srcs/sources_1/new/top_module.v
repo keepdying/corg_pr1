677c9cb91d0375f697981cd99814d1709dcd5ea1
keepdying <yunusemre34tr@gmail.com>
2022-05-08 01:27:16 +0300

    replaced auto-test files with fixed ones

--- corg_pr1.srcs/sources_1/new/top_module.v
+++ corg_pr1.srcs/sources_1/new/top_module.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/08/2022 12:52:37 AM
-// Design Name: 
-// Module Name: top_module
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module top_module(
 
